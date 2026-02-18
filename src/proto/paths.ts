185902b9298ca5543742360c25cd271021e12269
Egor312-33 <olegsergeon4@gmail.com>
2026-02-18 20:33:08 +0700

    create: media paths proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -6,4 +6,5 @@ export const PROTO_PATHS = {
   AUTH: join(ROOT_PATH, "auth.proto"),
   ACCOUNT: join(ROOT_PATH, "account.proto"),
   USERS: join(ROOT_PATH, "users.proto"),
+  MEDIA: join(ROOT_PATH, "media.proto"),
 } as const;
