ff31a55c2672e289f59940b9daf322a29fe06226
Egor312-33 <olegsergeon4@gmail.com>
2026-03-17 01:30:30 +0700

    fix: update import event auth

--- src/events/auth/index.ts
+++ src/events/auth/index.ts
@@ -1,3 +1,5 @@
-export * from "./otp-requested.interface";
 export * from "./create-system-chats.interface";
+export * from "./create-users.interface";
 export * from "./login-detected.interface";
+export * from "./otp-requested.interface";
+
